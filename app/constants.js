f61ba55098279fb53411f921dd9788ca9de79266
Bharathi Krishna V M <49585360+BharathiKrishnaVM@users.noreply.github.com>
2024-09-26 23:32:52 +0530

    Update constants.js

--- app/constants.js
+++ app/constants.js
@@ -1,6 +1,8 @@
-const MajorColorNames = [
+export class colorConstants {
+public static readonly MAJOR_COLOR_NAMES = [
     "WHITE", "RED", "BLACK", "YELLOW", "VIOLET"
 ];
-const MinorColorNames = [
+public static readonly MINOR_COLOR_NAMES = [
 	"BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
 ];
+}
