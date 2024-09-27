84e7945c4900f67c89c0a7bd57850b0c63b3978b
Bharathi Krishna V M <49585360+BharathiKrishnaVM@users.noreply.github.com>
2024-09-27 09:38:34 +0530

    Create manualColor.js

---
+++ app/manualColor.js
@@ -0,0 +1,16 @@
+import { MAJOR_COLOR_NAMES, MINOR_COLOR_NAMES, getColorFromPairNumber } from "./Utility";
+
+function manualColorCombination() {
+    const minorSize = MAJOR_COLOR_NAMES.length;
+    const majorSize = MINOR_COLOR_NAMES.length;
+    let manual = '';
+
+    for (let i = 1; i <= minorSize * majorSize; i++) {
+        const colorPair = getColorFromPairNumber(i);
+        manual += `Pair Number: ${i}, Colors: ${colorPair.toString()}\n`;
+    }
+
+    return manual;
+}
+
+module.exports = manualColorCombination;
