1aeb39e814bdf197e203e1f9ebd2b0e5280f5710
Bharathi Krishna V M <49585360+BharathiKrishnaVM@users.noreply.github.com>
2024-09-27 09:34:07 +0530

    Delete app/constants.js

--- app/constants.js
+++
@@ -1,8 +0,0 @@
-export class ColorConstants {
-public static readonly MAJOR_COLOR_NAMES = [
-    "WHITE", "RED", "BLACK", "YELLOW", "VIOLET"
-];
-public static readonly MINOR_COLOR_NAMES = [
-	"BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
-];
-}
