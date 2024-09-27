94e3543dbc75d1c0d849c10ad4a8c608a9c6ee63
Bharathi Krishna V M <49585360+BharathiKrishnaVM@users.noreply.github.com>
2024-09-27 09:36:10 +0530

    Update colorPairs.js

--- app/colorPairs.js
+++ app/colorPairs.js
@@ -1,18 +1,41 @@
-import { ColorConstants } from '../app/constants.js';
+import { getColorFromPairNumber } from "./Utility";
+import { getPairNumberFromColor } from "./Utility";
+import { manualColorCombination } from "./manualColor";
 
-function getColorFromPairNumber(pairNumber){
-	let minorSize = ColorConstants.MAJOR_COLOR_NAMES.length;
-	let majorSize = ColorConstants.MINOR_COLOR_NAMES.length;
-	
-	if (pairNumber < 1 || pairNumber > minorSize * majorSize)
- 	{
- 		throw `Argument PairNumber:${pairNumber} is outside the allowed range` 
-	}
-	let zeroBasedPairNumber = pairNumber - 1;
-    let majorIndex = parseInt (zeroBasedPairNumber / minorSize);
-    let minorIndex = parseInt(zeroBasedPairNumber % minorSize);
-	let  pair = new ColorPair();
-	pair.majorColor = ColorConstants.MAJOR_COLOR_NAMES[majorIndex];
-	pair.minorColor =  ColorConstants.MINOR_COLOR_NAMES[minorIndex];
-	return pair;
+function test() {
+    //Color code test cases
+    const testCasesForColors = [{ pairNumber: 4, expectedMajorColor: "WHITE", expectedMinorColor: "BROWN" },
+    { pairNumber: 5, expectedMajorColor: "WHITE", expectedMinorColor: "SLATEGRAY" },
+    { pairNumber: 23, expectedMajorColor: "RED", expectedMinorColor: "GREEN" }
+    ]
+
+    testCasesForColors.map(({ pairNumber, expectedMajorColor, expectedMinorColor }) => {
+        const colorPair = getColorFromPairNumber(pairNumber);
+        console.log(`[In]Pair Number: ${pairNumber},[Out] Colors:${colorPair}`);
+        console.assert(testPair1.majorColor == expectedMajorColor, `Expected major color is ${expectedMajorColor} but existing color is ${colorPair.majorColor}`);
+        console.assert(testPair1.minorColor == expectedMinorColor, `Expected minor color is ${expectedMinorColor} but existing color is ${colorPair.minorColor}`);
+    })
+   //Pair numbers of given color code
+    const testCasesForPairNumber = [
+        { majorColor: "YELLOW", minorColor: "GREEN", expectedPairNumber: 18 },
+        { majorColor: "RED", minorColor: "BLUE", expectedPairNumber: 6 }
+    ];
+
+    testCasesForPairNumber.forEach(({ majorColor, minorColor, expectedPairNumber }) => {
+        const colorPair = new ColorPair(majorColor, minorColor);
+        const pairNumber = getPairNumberFromColor(colorPair);
+        console.log(`[In] Colors: ${colorPair}, [Out] Pair Number: ${pairNumber}`);
+        console.assert(pairNumber === expectedPairNumber, `Expected Pair Number: ${expectedPairNumber}, but existing is: ${pairNumber}`);
+    });
 }
+//Manual color combinations
+function printColorCodingManual() {
+    console.log("Color Coding Manual:\n" + manualColorCombination());
+}
+
+function main() {
+    runTestCases();
+    printColorCodingManual();
+}
+
+main()
